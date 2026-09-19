f44c068fed19ef97b92ce56f69d445abd1f12641
germanguerra1987 <german.guerra.2030@gmail.com>
2026-09-18 23:59:14 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'easy-checklist-v1';
+const APP_SHELL = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => cachedResponse || fetch(event.request).then(networkResponse => {
+        if (networkResponse.ok && new URL(event.request.url).origin === self.location.origin) {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        }
+        return networkResponse;
+      }))
+      .catch(() => caches.match('./index.html'))
+  );
+});
